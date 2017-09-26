f06beea5d18abaf498d5b9a1280186d983a58229
Thinker222 <es0197@yahoo.com>
2017-09-26 15:52:05 -0400

    Update Driver.h

--- src/Driver.h
+++ src/Driver.h
@@ -5,14 +5,15 @@
 #ifndef OS_GROUP_PROJECT_DRIVER_H
 #define OS_GROUP_PROJECT_DRIVER_H
 
-//  The driver will have a loader, disk, ram, long term scheduler, log and ram objects. 
+//  The driver will have a loader, disk, ram, long term scheduler, log, CPU and ram objects. 
 // The Driver will have a function called run(), that will execute the OS, and 
 // run all of its overarching commands.   
 // The run function will execute several lower functions as follows: 
 // load.load(): Reads the program file into the disk
 // The next functions are run in a loop: 
-// scheduleNext(): Calls functions from long term scheduler and short term scheduler
-// 
+// scheduleNext(): Calls functions from long term scheduler and short term scheduler to load 
+// jobs onto the ready queue and the ram.
+//  
 
 
 class Driver {
