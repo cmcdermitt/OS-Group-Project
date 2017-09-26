0672d75ffdb3d763659f7571179cf80a50aa4e2b
zauzenne <32309357+zauzenne@users.noreply.github.com>
2017-09-26 15:54:18 -0400

    Update Disk.h

--- src/Disk.h
+++ src/Disk.h
@@ -5,6 +5,8 @@
 #ifndef OS_GROUP_PROJECT_DISK_H
 #define OS_GROUP_PROJECT_DISK_H
 
+//Comprised of an array limited to 2048 words (or locations)
+//Each location will hold a hex word "0x000000"
 
 class Disk {
 
