c54964ad15ee759e4d7afea7bd0810e472bb7872
Thinker222 <es0197@yahoo.com>
2017-10-03 16:41:55 -0400

    Loader stuff

--- src/Loader.cpp
+++ src/Loader.cpp
@@ -15,7 +15,14 @@
     void Loader::init (Disk *disk_to_load) {
         std::ifstream input(CODEFILENAME);
         std::string temp;
-        while(getline(input, temp));
+        int currentPosition = 0;
+        while(getline(input, temp))
+        {
+            // If the line is PCB Data
+
+            // Else it is hexcode
+
+        }
     }
         //logic for actually reading file goes here
 
