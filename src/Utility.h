2754490f18f4957224bf5b78b01d69ad3bd458ed
Alex-Denton <32309429+Alex-Denton@users.noreply.github.com>
2017-10-03 15:01:47 -0400

    Update Utility.h

--- src/Utility.h
+++ src/Utility.h
@@ -1,6 +1,10 @@
 #ifndef UTILITY_H_INCLUDED
 #define UTILITY_H_INCLUDED
 
-
+class Utility
+{
+  public:
+    __int32 HexToBinary( __int32 hex); //converts Hex values to 32-bit binary long
+}
 
 #endif // UTILITY_H_INCLUDED
