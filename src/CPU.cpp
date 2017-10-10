18ec8313b34746c98b247c5f5907f86e6bdf69c7
xepher0coded <eshin@me.com>
2017-10-10 15:09:28 -0400

    Fixed CPU returns + Utility pow reference

--- src/CPU.cpp
+++ src/CPU.cpp
@@ -22,10 +22,12 @@ bool CPU::WR() {
 
 bool CPU::ST(int addr, int regNum) {
     this ->ram.write(addr,this->Register[regNum]);
+    return true;
 }
 
 bool CPU::LW(int addr, int regNum) {
     this->Register[regNum]=this->ram.read(addr);
+    return true;
 }
 
 bool CPU::MOV(int fromReg, int toReg) {
@@ -35,42 +37,55 @@ bool CPU::MOV(int fromReg, int toReg) {
 }
 bool CPU::ADD(int S1, int S2, int D){
     this->Register[D] = this->Register[S1] + this->Register[S2];
+    return true;
 }
 bool CPU::SUB(int S1, int S2, int D){
     this->Register[D] = this->Register[S1] - this->Register[S2];
+    return true;
 }
 bool CPU::MUL(int S1, int S2, int D){
     this->Register[D] = this->Register[S1] * this->Register[S2];
+    return true;
 }
 bool CPU::DIV(int S1, int S2, int D){
     this->Register[D] = this->Register[S1] / this->Register[S2];
+    return true;
 }
 bool CPU::AND(int S1, int S2, int D){
     this->Register[D] = this->Register[S1] & this->Register[S2];
+    return true;
 }
 bool CPU::OR(int S1, int S2, int D){
     this->Register[D] = this->Register[S1] | this->Register[S2];
+    return true;
 }
 bool CPU::MOVI(int val, int toReg){
     this -> Register[toReg] = val;
+    return true;
 }
 bool CPU::ADDI(int val, int D){
     this->Register[D]+= val;
+    return true;
 }
 bool CPU::MULI(int val, int D){
     this->Register[D]*= val;
+    return true;
 }
 bool CPU::DIVI(int val, int D){
     this->Register[D]/= val;
+    return true;
 }
 bool CPU::LDI(int val, int D){
     this->Register[D]= val;
+    return true;
 }
 bool CPU::SLT(int S, int B, int D){
     this->Register[D]= (this->Register[S]<this->Register[B])?1:0;
+    return true;
 }
 bool CPU::SLTI(int S, int val, int D){
     this->Register[D]= (this->Register[S]<val)?1:0;
+    return true;
 }
 bool CPU::HLT(){
     return true; //end program?
@@ -83,21 +98,27 @@ bool CPU::JMP(int lineNo){
 }
 bool CPU::BEQ(int B, int D, int addr){
     if(this->Register[B]==this->Register[D]) return true; // What is addr?
+    return true;
 }
 bool CPU::BNE(int B, int D, int addr){
     if(this->Register[B]!=this->Register[D]) return true; // What is addr?
+    return true;
 }
 bool CPU::BEZ(int B, int addr){
     if(this->Register[B]==0) return true; // What is addr?
+    return true;
 }
 bool CPU::BNZ(int B, int addr){
     if(this->Register[B]!=0) return true; // What is addr?
+    return true;
 }
 bool CPU::BGZ(int B, int addr){
     if(this->Register[B]>0) return true; // What is addr?
+    return true;
 }
 bool CPU::BLZ(int B, int addr){
     if(this->Register[B]<0) return true; // What is addr?
+    return true;
 }
 //int* CPU::dump_registers() const{
 //    return Register;
