12d3a9b3949a6bef5f5a845ad873d5f617435ea7
Thinker222 <es0197@yahoo.com>
2017-10-17 15:18:36 -0400

    Update Ram.cpp

--- src/Ram.cpp
+++ src/Ram.cpp
@@ -2,26 +2,30 @@
 // Created by Eshin Griffith on 9/26/17.
 //
 
-#include "Ram.h"
+#include "RAM.h"
 #include <iostream>
 #include <stdlib.h>
  #include <stdint.h>
 
     void RAM::write(int address, std::string data)
     {
+        mutex.lock();
         if(address >= SIZE || address < 0)
             exit(EXIT_FAILURE);
         ram_data[address] = data;
-
+        mutex.unlock();
     }
 
     std::string RAM::read(int address)
     {
+        mutex.lock();
         return ram_data[address];
+        mutex.unlock(); 
     }
 
     RAM::RAM()
     {
+        mutex = std::mutex();
         for(int i = 0; i < SIZE; i++)
             ram_data[i] = "";
     }
