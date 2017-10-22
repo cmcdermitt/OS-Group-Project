32a7d1226e7e00d31ebf0b6c4724c42125ac1746
cmcdermitt <charliemc1996@gmail.com>
2017-10-22 04:19:28 -0400

    Rewrote entire project in Java

---
+++ src/Cache.h
@@ -0,0 +1,30 @@
+//
+// Created by charlie on 10/22/17.
+//
+
+#ifndef OS_GROUP_PROJECT_CACHE_H
+#define OS_GROUP_PROJECT_CACHE_H
+
+#include <string>
+#include <vector>
+#include "Log.h"
+
+
+class Cache {
+
+
+public:
+    std::string read(int address);
+    void write(int address, std::string data);
+    void write(int address, std::vector<std::string> s);
+    const static int SIZE = 128;
+    Log *cache_log = new Log("Cache_Log");
+    Cache();
+    ~Cache();
+
+private:
+    std::string cache_data[SIZE];
+};
+
+
+#endif //OS_GROUP_PROJECT_CACHE_H
